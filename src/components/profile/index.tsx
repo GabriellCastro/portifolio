70c68cdb18d7e2acd769b68aaa91a209af281dd8
Gabriel <kastro.gabriel17@gmail.com>
2022-07-25 20:10:44 -0400

    feat: profile component

--- src/components/profile/index.tsx
+++ src/components/profile/index.tsx
@@ -1,5 +1,21 @@
+import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 import { FC } from "react";
 
 export const Profile: FC = () => {
-  return <div>Profile</div>;
+  return (
+    <Box>
+      <Flex direction="column">
+        <Avatar
+          name="Gabriel"
+          src="https://avatars.githubusercontent.com/u/61993679?s=400&u=05636c7741f20aeb23fba2f82f05a30d3de231e8&v=4"
+          size="2xl"
+          borderRadius="md"
+          marginBottom="12px"
+        />
+        <Text marginBottom="32px" fontSize="1.5rem">
+          Hey there, {`I'm Gabriel`}
+        </Text>
+      </Flex>
+    </Box>
+  );
 };
