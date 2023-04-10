efce66f77a60e889060a2a849a96365824bbecff
gabriellcastro <kastro.gabriel17@gmail.com>
2023-04-10 17:40:07 -0300

    refactor: profile change styles and links

--- src/components/profile/index.tsx
+++ src/components/profile/index.tsx
@@ -1,20 +1,46 @@
-import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
+import { Avatar, Box, Flex, Icon, Text } from "@chakra-ui/react";
 import { FC } from "react";
+import { FiGithub, FiInstagram, FiLinkedin, FiTwitter } from "react-icons/fi";
 
 export const Profile: FC = () => {
   return (
-    <Box>
-      <Flex direction="column">
-        <Avatar
-          name="Gabriel"
-          src="https://avatars.githubusercontent.com/u/61993679?s=400&u=05636c7741f20aeb23fba2f82f05a30d3de231e8&v=4"
-          size="2xl"
-          borderRadius="md"
-          marginBottom="12px"
-        />
-        <Text marginBottom="32px" fontSize="1.5rem">
-          Hey there, {`I'm Gabriel`} 🧑🏻‍💻
-        </Text>
+    <Box mt="16" mb="16">
+      <Flex direction="row" alignItems="center">
+        <Box borderRadius="full" boxShadow="0 0 0 3px #fff, 0 0 0 6px #FFE071">
+          <Avatar
+            name="Gabriel"
+            src="https://avatars.githubusercontent.com/u/61993679?s=400&u=05636c7741f20aeb23fba2f82f05a30d3de231e8&v=4"
+            size="2xl"
+            borderRadius="full"
+          />
+        </Box>
+        <Flex direction="column">
+          <Text fontSize="1.5rem" ml="8" fontWeight="semibold">
+            Gabriel Castro 🧑🏻‍💻
+          </Text>
+          <Text fontSize="1rem" ml="8" color="gray.400">
+            Full-Stack Developer
+          </Text>
+          <Flex direction="row" mt="3" ml="8" gap={4}>
+            <FiGithub
+              fontSize="24px"
+              onClick={() =>
+                window.open("https://github.com/gabriellcastro", "_blank")
+              }
+              cursor="pointer"
+            />
+            <FiLinkedin
+              fontSize="24px"
+              cursor="pointer"
+              onClick={() =>
+                window.open(
+                  "https://www.linkedin.com/in/eugabrielcastro/",
+                  "_blank"
+                )
+              }
+            />
+          </Flex>
+        </Flex>
       </Flex>
     </Box>
   );
