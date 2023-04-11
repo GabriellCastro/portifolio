7832ee3ab3c3d97b2286b0869b6b66ed912b1560
gabriellcastro <kastro.gabriel17@gmail.com>
2023-04-10 21:36:08 -0300

    feat: container center

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,21 @@
 import { Container } from "@chakra-ui/react";
 import type { NextPage } from "next";
 import { Profile } from "~/components/profile";
-import { SocialButton } from "~/components/socialButton";
+import { ButtonTheme } from "~/components/buttonTheme";
 
 const Home: NextPage = () => {
   return (
-    <Container maxW="container.xl" centerContent>
+    <Container
+      maxW="container.xl"
+      centerContent
+      h="100vh"
+      display="flex"
+      flexDirection="column"
+      justifyContent="center"
+      alignItems="center"
+    >
       <Profile />
-      <SocialButton />
+      <ButtonTheme />
     </Container>
   );
 };
